6047bb1a4ff324724b6fc0543f154e9ae0163517
Simon <Simon@10.16.237.254>
2015-10-22 17:08:39 +0200

    Når programmet køres åbnes et tomt vindue.

--- OOAD/src/gui/GUI.java
+++ OOAD/src/gui/GUI.java
@@ -1,12 +1,21 @@
 package gui;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class GUI {
 	
 	JFrame window;
+	
+	public GUI() {
+		window = new JFrame("DILEMMA");
+	}
 
 	public void openWindow() {
-		window = new JFrame();
+		window.setPreferredSize(new Dimension(400, 600));
+		window.pack();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
 	}
 }

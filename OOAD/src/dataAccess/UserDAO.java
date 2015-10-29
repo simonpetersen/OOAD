32801dd859c260be09ec552232548297a2a96e17
Sandie <sandieAkkermans@hotmail.com>
2015-10-29 16:03:58 +0100

    Opdateret

--- OOAD/src/dataAccess/UserDAO.java
+++ OOAD/src/dataAccess/UserDAO.java
@@ -3,6 +3,7 @@ package dataAccess;
 public interface UserDAO {
 
 	UserDAO getUser(int userID) throws DALException;
+	UserDAO getUserName(String userName) throws DALException;
 	UserDAO getPasword(String pasword) throws DALException;
 	UserDAO getEmail(String email) throws DALException;
 }
--- OOAD/src/dataAccess/UserDTO.java
+++ OOAD/src/dataAccess/UserDTO.java
@@ -3,10 +3,12 @@ package dataAccess;
 public class UserDTO implements UserDAO {
 
 	public UserDAO getUser(int userID) throws DALException {
-		
 		return null;
 	}
-	public UserDAO getPasword(String pasword) throws DALException {
+	public UserDAO getUserName(String userName) throws DALException{
+		return null;
+	}
+	public UserDAO getPasword(String pasword) throws DALException {	
 		return null;
 	}
 	public UserDAO getEmail(String email) throws DALException {

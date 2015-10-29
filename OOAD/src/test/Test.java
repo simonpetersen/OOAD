7ff83e3a3b5d2271a0666163bdd32bc24913c39a
Simon <Simon@dipl-l0191.win.dtu.dk>
2015-10-29 16:21:39 +0100

    Ændret i test-metoden.

--- OOAD/src/test/Test.java
+++ OOAD/src/test/Test.java
@@ -1,6 +1,6 @@
 package test;
 
-import java.util.Calendar;
+import java.math.BigInteger;
 import java.util.Date;
 
 public class Test {
@@ -9,8 +9,13 @@ public class Test {
 		Date d1 = new Date();
 		System.out.println(d1);
 		System.out.println(d1.getTime());
-		Date d2 = new Date(d1.getTime());
+		Date d2 = new Date(d1.getTime()+(60*60*1000));
 		System.out.println(d2);
+		Date d3 = new Date(d2.getTime());
+		System.out.println(d3);
+		System.out.println(d3.getTime());
+		BigInteger bInt = new BigInteger("0");
+		bInt.
 	}
 
 }

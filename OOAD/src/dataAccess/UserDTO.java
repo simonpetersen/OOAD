e131225bf3085da47d5f0ff3b5d0d8cd68cd182c
Sandie <sandieAkkermans@hotmail.com>
2015-10-29 16:06:00 +0100

    -

--- OOAD/src/dataAccess/UserDTO.java
+++ OOAD/src/dataAccess/UserDTO.java
@@ -1,6 +1,9 @@
 package dataAccess;
 
 public class UserDTO implements UserDAO {
+	
+	int userID;
+	String userName, pasword, email;
 
 	public UserDAO getUser(int userID) throws DALException {
 		return null;

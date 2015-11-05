2ca0a1d97802358944d7f2825fe0ae01b19228a5
Simon <simon_petersen1994@hotmail.com>
2015-11-05 15:31:56 +0100

    Fix: De foreløbige GUI-elementer bliver nu tilføjet til JFrame.

--- OOAD/src/gui/GUI.java
+++ OOAD/src/gui/GUI.java
@@ -2,7 +2,9 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -20,12 +22,18 @@ public class GUI {
 	JPanel categoryPanel;
 	JPanel saveButtonPanel;
 	JPanel seriousnessPanel;
+	JPanel seriousLabelPanel;
 	
 	//Initialisering af Label til forklarende tekst, samt drop-down boxes for kategori og seriøsitet.
 	JLabel seriousLabel;
 	JLabel categoryLabel;
 	JComboBox<String> seriousChoices;
 	JComboBox<String> categoryChoices;
+	JButton seriousLevel1;
+	JButton seriousLevel2;
+	JButton seriousLevel3;
+	JButton seriousLevel4;
+	JButton seriousLevel5;
 	
 	//Initialisering af tekstfelter til overskrift og beskrivelse af dilemma, samt knap til at gemme.
 	JTextField titleField;
@@ -38,9 +46,18 @@ public class GUI {
 		titleField = new JTextField("Overskrift", 30);
 		descriptionTextArea = new JTextArea("Beskrivelse", 8, 30);
 		saveButton = new JButton("Gem");
-		seriousLabel = new JLabel("Vælg seriøsitet: ");
+		seriousLabel = new JLabel("Seriøsitet: ");
 		categoryLabel = new JLabel("Vælg kategori: ");
-		seriousnessPanel = new JPanel();
+		
+		seriousLevel1 = new JButton("1");
+		seriousLevel2 = new JButton("2");
+		seriousLevel3 = new JButton("3");
+		seriousLevel4 = new JButton("4");
+		seriousLevel5 = new JButton("5");
+		
+		seriousnessPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		seriousLabelPanel = new JPanel();
+		categoryPanel = new JPanel();
 		descriptionPanel = new JPanel();
 		titlePanel = new JPanel();
 		descriptionPanel = new JPanel();
@@ -52,23 +69,30 @@ public class GUI {
 		window.pack();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
+		window.getContentPane().setLayout(new BoxLayout(window.getContentPane(), BoxLayout.Y_AXIS));
 		
 		descriptionTextArea.setLineWrap(true);
 		descriptionPanel.add(descriptionTextArea);
 		
-		/*
-		String[] opt = {"1", "2", "3", "4", "5"};
-		seriousChoices = new JComboBox<String>(opt);
-		seriousnessPanel.add(seriousLabel);
-		seriousnessPanel.add(seriousChoices);
-		*/
+		//String[] opt = {"1", "2", "3", "4", "5"};
+		//seriousChoices = new JComboBox<String>(opt);
+		seriousLabelPanel.add(seriousLabel);
+		seriousnessPanel.add(seriousLevel1);
+		seriousnessPanel.add(seriousLevel2);
+		seriousnessPanel.add(seriousLevel3);
+		seriousnessPanel.add(seriousLevel4);
+		seriousnessPanel.add(seriousLevel5);
 		
 		saveButtonPanel.add(saveButton);
 		titlePanel.add(titleField);
 		
-		window.getContentPane().add(titlePanel, BorderLayout.NORTH);
-//		window.getContentPane().add(seriousnessPanel, BorderLayout.SOUTH);
-		window.add(descriptionPanel, BorderLayout.CENTER);
-		window.getContentPane().add(saveButtonPanel, BorderLayout.SOUTH);
+		
+		window.add(titlePanel);
+		window.add(descriptionPanel);
+		window.add(seriousLabelPanel);
+		window.add(seriousnessPanel);
+		window.add(saveButtonPanel, BorderLayout.SOUTH);
+		
+		
 	}
 }

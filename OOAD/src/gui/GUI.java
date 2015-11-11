8b8b8121fa309d2427b1a0a372ebf6bcfbf1a200
Simon <simon_petersen1994@hotmail.com>
2015-11-11 20:52:42 +0100

    Tilføjet ActionPerformed til GUI.

--- OOAD/src/gui/GUI.java
+++ OOAD/src/gui/GUI.java
@@ -2,6 +2,8 @@ package gui;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -14,7 +16,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
 
-public class GUI {
+public class GUI implements ActionListener {
 	
 	//Initialisering af vindue og JPanels der holder hvert element i interfacet.
 	JFrame window;
@@ -50,11 +52,14 @@ public class GUI {
 		titleField = new JTextField("Overskrift", 30);
 		descriptionTextArea = new JTextArea("Beskrivelse", 8, 30);
 		saveButton = new JButton("Gem");
+		saveButton.addActionListener(this);
 		seriousLabel = new JLabel("Seriøsitet: ");
 		categoryLabel = new JLabel("Vælg kategori: ");
 		timeLabel = new JLabel("Svartid: ");
 		
 		seriousLevel1 = new JButton("1");
+		seriousLevel1.addActionListener(this);
+		seriousLevel1.setActionCommand("Serious = 1");
 		seriousLevel2 = new JButton("2");
 		seriousLevel3 = new JButton("3");
 		seriousLevel4 = new JButton("4");
@@ -107,5 +112,16 @@ public class GUI {
 		window.add(categoryPanel);
 		window.add(timePanel);
 		window.add(saveButtonPanel);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if ("Serious = 1".equals(e.getActionCommand()))
+			System.out.println(seriousLevel1.getText());
+		else {
+			System.out.println("Title: "+titleField.getText());
+			System.out.println("Description: "+descriptionTextArea.getText());
+			System.out.println("Category: "+categoryOpt[categoryChoices.getSelectedIndex()]);
+			System.out.println("Time: "+timeSpinner.getValue());
+		}
 	}
 }

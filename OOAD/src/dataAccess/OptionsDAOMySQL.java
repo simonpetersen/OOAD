0fe94c1d853c5999a394556774c21304c84c3d62
Simon <simon_petersen1994@hotmail.com>
2015-11-19 16:31:20 +0100

    Fix: OptionsDB også tilpasset logisk skema.

--- OOAD/src/dataAccess/OptionsDAOMySQL.java
+++ OOAD/src/dataAccess/OptionsDAOMySQL.java
@@ -7,8 +7,8 @@ public class OptionsDAOMySQL implements OptionsDAO {
 	@Override
 	public void saveOptions(ArrayList<String> options, int dilemmaID) throws DALException {
 		for (int i=0; i<options.size(); i++) {
-			Connector.doUpdate("INSERT into options VALUES("+dilemmaID+", "+(i+1)+
-					", "+options.get(i)+", "+0);
+			Connector.doUpdate("INSERT into options VALUES("+(i+1)+", "
+					+dilemmaID+", "+", "+options.get(i)+", "+0);
 		}
 	}
 

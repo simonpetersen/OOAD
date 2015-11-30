db36520f1cfd5bf14add3939a57d1bf7280daf94
Simon <simon_petersen1994@hotmail.com>
2015-11-30 09:27:38 +0100

    Tilføjet DilemmaTest-klasse.

---
+++ OOAD/src/test/DilemmaTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DilemmaTest {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+	
+	@Test
+	public void testSaveDilemma() {
+		
+	}
+
+}

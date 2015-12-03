e45c6cc240110ca567a18dfe7cd41ea001400713
Simon <simon_petersen1994@hotmail.com>
2015-12-03 13:43:36 +0100

    Tilføjet metode til Options-interface.

--- OOAD/src/dataAccess/OptionsDAO.java
+++ OOAD/src/dataAccess/OptionsDAO.java
@@ -5,4 +5,5 @@ import java.util.ArrayList;
 public interface OptionsDAO {
 	void saveOptions(ArrayList<String> Options, int dilemmaID) throws DALException;
 	int getNextOptionID() throws DALException;
+	public ArrayList<String> getOptions(int dilemmaID) throws DALException;
 }
